29371f7734e03c31149dd6583898f3e168ab9b59
Olaf Kranse <54064523+OlafKranse@users.noreply.github.com>
2021-01-09 00:12:16 +0000

    Update finding_all_unique_sequences_in_fasta.py

--- finding_all_unique_sequences_in_fasta.py
+++ finding_all_unique_sequences_in_fasta.py
@@ -1,20 +1,25 @@
 ##make a file of unqiue possible sequences in a given range (change sequence size)
-sequence_size = 24
+### setup ###
+sequence_size = 3
+additional_ranges = 1
+
+### main script ###
+sequence_size -= 1
 file = open("output", "w")
 file.close()
 
-from itertools import combinations_with_replacement
+from itertools import product
 
-alphabets = ["a", "t", "c", "g"]
+alphabets = "atcg"
 
 
 input_range = enumerate(map(int, range(sequence_size)))
 
 
-for i in range(6):
+for i in range(additional_ranges):
     sequence_size += 1
     with open("output", "a") as f:
-        for input_range in combinations_with_replacement(alphabets, sequence_size):
+        for input_range in list(product(alphabets, repeat=sequence_size)):
             writeme = "".join(input_range)
             f.write(writeme+ "\n")
 
